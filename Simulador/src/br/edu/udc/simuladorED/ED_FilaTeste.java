08de36979662701c4fc9b6e50b7af08af0146c7c
ayrto <ayrto@DESKTOP-BL7MK5R>
2016-09-08 08:18:39 -0300

    adicionei teste em loop de 300

--- Simulador/src/br/edu/udc/simuladorED/ED_FilaTeste.java
+++ Simulador/src/br/edu/udc/simuladorED/ED_FilaTeste.java
@@ -2,6 +2,29 @@ package br.edu.udc.simuladorED;
 
 public class ED_FilaTeste {
 	public static void main(String[] args) {
+
+	}
+
+	public void test() {
+		ED_Fila<String> lista = new ED_Fila<String>();
+
+		for (int i = 0; i < 300; i++) {
+			lista.adiciona("1");
+			lista.adiciona("2");
+			lista.adiciona("3");
+
+		}
+
+		for (int i = 0; i < 300; i++) {
+			// if(i % 2 == 0 || i % 3 == 1)// teste de remocao durante a
+			// impressao se não remover e a mesma
+			// lista.remover(); //coisa sempre e se remover ah variacao...
+
+			System.out.println(lista.consulta());
+		}
+	}
+
+	public void adicionasucesso() {
 		ED_Fila<String> lista = new ED_Fila<String>();
 
 		lista.adiciona("olamundo");
@@ -10,10 +33,5 @@ public class ED_FilaTeste {
 		lista.adiciona("olamundoo");
 		System.out.println(lista.consulta());
 		System.out.println(lista.tamanho());
-
-	}
-
-	public void adicionasucesso() {
-
 	}
 }
