29a49c2d61f62041502e319b88e40d6cd71466af
ayrto <ayrto@PCAGK>
2016-11-27 12:00:44 -0200

    implementando comunicaçao com computador

--- Simulador/src/br/edu/udc/simulador/janela/FrameMemoria.java
+++ Simulador/src/br/edu/udc/simulador/janela/FrameMemoria.java
@@ -8,7 +8,6 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
 
-import br.edu.udc.simulador.Computador;
 import br.edu.udc.simulador.janela.view.AttView;
 import br.edu.udc.simulador.janela.view.ViewMemoria;
 
@@ -16,7 +15,6 @@ public class FrameMemoria extends JFrame implements AttView{
 	private static final long serialVersionUID = 1L;
 	private ViewMemoria viewMemoria;
 	private JPanel contentPane;
-	private Computador computador;
 	private JScrollPane scrollpane;
 	
 	/**
@@ -41,13 +39,12 @@ public class FrameMemoria extends JFrame implements AttView{
 	public FrameMemoria() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
-		computador = new Computador();
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		scrollpane = new JScrollPane();
-		viewMemoria = new ViewMemoria(computador);
+		viewMemoria = new ViewMemoria();
 		scrollpane.setViewportView(viewMemoria);
 		viewMemoria.setPreferredSize(new Dimension(800,600));
 		contentPane.add(scrollpane);
